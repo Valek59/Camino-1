325d094a985863e42cd838832ebb8f44add274bf
(Valentin Styza) <styzavalentin@gmail.com>
2023-04-27 10:12:15 +0200

    adding root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  root "clients#developpement"
+  root to: "clients#developpement"
   resources :clients, only: [:new, :create]
   get '/developpement', to: 'clients#developpement', as: "developpement"
   get '/mentions-legales', to: 'clients#mentions_legales', as: "mentions_legales"
