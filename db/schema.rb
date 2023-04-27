1f5921f60748d306ec43030b78811f2a03e48e8c
(Valentin Styza) <styzavalentin@gmail.com>
2023-04-27 15:53:00 +0200

    transfer to sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_02_153555) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  create_table "clients", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "company"
-    t.string "role"
-    t.string "pro_email"
-    t.string "contact_preference"
-    t.string "phone_number"
-    t.string "how_know_camino"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+
 end
