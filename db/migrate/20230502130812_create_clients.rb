9d5a976514ce5b88e1ee81ede8ce878f8f2546ee
(Valentin Styza) <styzavalentin@gmail.com>
2023-05-02 15:13:39 +0200

    nouveau model

--- db/migrate/20230502130812_create_clients.rb
+++ db/migrate/20230502130812_create_clients.rb
@@ -1,14 +1,13 @@
 class CreateClients < ActiveRecord::Migration[7.0]
-  def changes
+  def change
     create_table :clients do |t|
       t.string :first_name
       t.string :last_name
       t.string :company
       t.string :role
       t.string :pro_email
-      t.string :contact_preference
       t.string :phone_number
-      t.string :how_know_camino
+
       t.timestamps
     end
   end
