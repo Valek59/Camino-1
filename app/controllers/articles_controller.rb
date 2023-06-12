783814e6a3acf46f9a7abc71a938a76992b11350
(Valentin Styza) <styzavalentin@gmail.com>
2023-06-12 15:53:46 +0200

    blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,15 +7,6 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def new
-    @article = Article.new
-  end
-
-  def create
-    article = Article.create(article_params)
-    redirect_to article
-  end
-
   private
 
   def article_params
